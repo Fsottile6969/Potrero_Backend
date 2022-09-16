1c7a727c6f6d1f1d2c88fee9961e96270bf73116
Francisco Sottile <fsottile6969@gmail.com>
2022-09-16 18:47:59 -0300

    creacion de formulairo de registro de usuario

---
+++ login/login.php
@@ -0,0 +1,13 @@
+<?php
+$usuario = $_POST['usuario'];
+$contrasenia = $_POST['password'];
+
+$ckuser = 'admin';
+$ckpass = '1234';
+
+if ($usuario == $user && $contrasenia == $ckpass) {
+	header ('Location:https://fsottile6969.github.io/PotreroDigital/');
+} else {
+	header ('Location:login.html');
+}
+?>
