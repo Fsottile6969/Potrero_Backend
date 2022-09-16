0c1e90513d2ab69f2ffbd66d4c31c3d474e158e4
Francisco Sottile <fsottile6969@gmail.com>
2022-09-16 19:26:54 -0300

    actualizacion

--- login/login.php
+++ login/login.php
@@ -7,7 +7,8 @@ $ckpass = '1234';
 
 if ($usuario == $user && $contrasenia == $ckpass) {
 	header ('Location:https://fsottile6969.github.io/PotreroDigital/');
-} else {
-	header ('Location:login.html');
+} 
+else {
+	header ('Location:error.html');
 }
 ?>
