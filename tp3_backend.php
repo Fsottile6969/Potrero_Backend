ef7170ad1287f9f898f0e660e3a060a5a85b467e
Francisco Sottile <fsottile6969@gmail.com>
2022-09-27 11:26:12 -0300

    actualizacion

--- tp3_backend.php
+++ tp3_backend.php
@@ -1,49 +1,56 @@
-<?php 
-// Mostrar los números del 1 al 100.
+<!-- Mostrar los números del 1 al 100. -->
+<!-- 
+<?php
 $i = 1;
 while ($i <= 100) {
 	print" <p>$i</p>\n";
 	$i++;
 }
-?>
+?> -->
+
+
+<!-- Mostrar los números del 100 al 1. -->
 
 <?php
-// Mostrar los números del 100 al 1.
- $i = 100;
- while ($i == 1) {
+$i = 100;
+while ($i >= 1) {
  	print" <p>$i</p>\n";
  	$i--;
 }
 ?>
 
-<?php
-// Mostrar los números pares del 1 al 100.
-for ($i = 0; $i = 100; $i+2) {
+<!-- Mostrar los números pares del 1 al 100. -->
+
+<!-- <?php
+for ($i = 0; $i <= 100; $i+2) {
  	print " <p>$i</p>\n";
 }
-?>
+?> -->
 
-<?php
-// Mostrar los números impares del 1 al 100.
+<!-- Mostrar los números impares del 1 al 100. -->
+
+<!-- <?php
 for ($i = 0; $i = 100; $i+3){
 	print " <p>$i</p>\n";
 }
-?>
+?> -->
 
-<?php
-// Mostrar la suma de los números de 1 a 20.
+<!-- Mostrar la suma de los números de 1 a 20. -->
+
+<!-- <?php
 $resultado = 0;
 $i = 0;
 while ($i = 20) {
 	($i++) + $resultado;
  	print "$resultado";
 }
-?>
+?> -->
 
-<?php
-// Mostrar la suma de los números pares de 1 a 20.
+<!-- Mostrar la suma de los números pares de 1 a 20. -->
+
+<!-- <?php
 $resultado = $i;
 for ($i= 0; $i = 20; $i+2) {
 	print "$resultado";
 }
-?>
+?> -->
