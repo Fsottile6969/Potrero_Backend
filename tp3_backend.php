4c5e8a49bb35dac2f08df9747288633766c5b80c
Francisco Sottile <fsottile6969@gmail.com>
2022-09-27 10:08:29 -0300

    creacion de tp3

---
+++ tp3_backend.php
@@ -0,0 +1,49 @@
+<?php 
+// Mostrar los números del 1 al 100.
+$i = 1;
+while ($i <= 100) {
+	print" <p>$i</p>\n";
+	$i++;
+}
+?>
+
+<?php
+// Mostrar los números del 100 al 1.
+ $i = 100;
+ while ($i == 1) {
+ 	print" <p>$i</p>\n";
+ 	$i--;
+}
+?>
+
+<?php
+// Mostrar los números pares del 1 al 100.
+for ($i = 0; $i = 100; $i+2) {
+ 	print " <p>$i</p>\n";
+}
+?>
+
+<?php
+// Mostrar los números impares del 1 al 100.
+for ($i = 0; $i = 100; $i+3){
+	print " <p>$i</p>\n";
+}
+?>
+
+<?php
+// Mostrar la suma de los números de 1 a 20.
+$resultado = 0;
+$i = 0;
+while ($i = 20) {
+	($i++) + $resultado;
+ 	print "$resultado";
+}
+?>
+
+<?php
+// Mostrar la suma de los números pares de 1 a 20.
+$resultado = $i;
+for ($i= 0; $i = 20; $i+2) {
+	print "$resultado";
+}
+?>
