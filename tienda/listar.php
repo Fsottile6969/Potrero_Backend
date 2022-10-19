73dfccdfd47de8505a94a0af2dae34a3a51200f7
Francisco Sottile <fsottile6969@gmail.com>
2022-10-19 15:00:51 -0300

    realizacion de lista y conexion con db

---
+++ tienda/listar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Tienda de ropa</h1>
+	<button type="submit"><a href="index.html"></a></button>
+	<button type="submit"><a href="listar.php"></a></button>
+	<h2>Lista de ropa</h2>
+	<p>Ropa en stock</p>
+	<table border="1">
+	<tr>
+		<th>ID</th>
+		<th>TIPO DE PRENDA</th>
+		<th>MARCA</th>
+		<th>TALLE</th>
+		<th>PRECIO</th>
+	</tr>
+
+	<?php
+	//Conexión con la base de datos
+	mysqli_connect("127.0.0.1", "root", "");
+	mysqli_select_db($conexión, `ropa`);
+
+	//Preparar la orden SQL
+
+	$consulta= "SELECT * FROM ropa";
+
+	//Ejecutar la orden y obtener los registros
+
+	$datos= mysqli_query($conexión, $consulta);
+
+	//Los datos de la base los convierte en un array y los guarda en $fila
+	
+	$fila= mysqli_fetch_array($datos);
+
+	//Mostrar los datos del registro
+
+	while ($fila) { ?>
+		<tr>
+		<td><?php echo $reg['id']; ?></td>
+		<td><?php echo $reg['tipo_prenda']; ?></td>
+		<td><?php echo $reg['marca']; ?></td>
+		<td><?php echo $reg['talle']; ?></td>
+		<td><?php echo $reg['precio']; ?></td>
+		</tr>
+	<?php }	?>
+	</table>
+</body>
+</html>
