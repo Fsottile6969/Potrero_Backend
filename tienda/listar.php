3afe52d574bb93dc425c6c44a97ae94fa1556193
Francisco Sottile <fsottile6969@gmail.com>
2022-10-19 21:43:43 -0300

    correction

--- tienda/listar.php
+++ tienda/listar.php
@@ -45,7 +45,7 @@
 		<td><?php echo $reg['marca']; ?></td>
 		<td><?php echo $reg['talle']; ?></td>
 		<td><?php echo $reg['precio']; ?></td>
-		<td><?php echo base64_encode ($reg['foto']); ?></td>
+		<td><img src="data:image/png;base64, <?php echo base64_encode($reg['imagen'])?>" alt="" width="100px" height="100px"></td>
 		</tr>
 	<?php } ?>
 	</table>
