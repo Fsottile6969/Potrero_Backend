904c9914fa4cb05f0f7020fa7594d841f09dab17
Francisco Sottile <fsottile6969@gmail.com>
2022-10-22 22:10:02 -0300

    correcciones

--- tienda/listar.php
+++ tienda/listar.php
@@ -7,7 +7,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Document</title>
 </head>
-<body class="body overflow-hidden">
+<body class="body overflow-auto">
 	<h1 class="titlee text-center">Tienda de ropa</h1>
 	<div class="row">
 		<div class="col d-flex flex-row">
