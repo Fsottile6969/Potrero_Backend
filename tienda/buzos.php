12a22b16d8ff3821c825afdadc9b62d78c5fafde
Francisco Sottile <fsottile6969@gmail.com>
2022-10-23 11:38:14 -0300

    correcciones

--- tienda/buzos.php
+++ tienda/buzos.php
@@ -73,14 +73,14 @@
 			//Mostrar los datos del registro
 
 			while ($reg= mysqli_fetch_array($datos)) { ?>
-				<div class="card mx-3 mt-3" style="width: 224px, height: 330px";>
- 					<img class="card-img-top" src="data:image/png;base64, <?php echo base64_encode($reg['foto'])?>" alt="" width="100px" height="150px">
-  					<div class="card-body">
-    					<p class="card-text d-inline-block"><?php echo $reg['tipo_prenda']; ?></p>
-    					<p class="card-text d-inline-block"><?php echo $reg['marca']; ?></p><br>
+				<div class="card m-3 p-2" style="width: 12rem; min-width: 12rem; height: 330px";>
+					<img class="card-img-top p-0" src="data:image/png;base64, <?php echo base64_encode($reg['foto'])?>" alt="" width="224px" height="224px">
+					<div class="card-body">
+   						<p class="card-text d-inline-block"><?php echo $reg['tipo_prenda']; ?></p>
+   						<p class="card-text d-inline-block"><?php echo $reg['marca']; ?></p><br>
 						<p class="card-text d-inline-block"><?php echo $reg['talle']; ?></p>
 						<p class="card-text d-inline-block"><?php echo $reg['precio']; ?></p>
-  					</div>								
+					</div>								
 				</div>
 			<?php } ?>
 		</div>
