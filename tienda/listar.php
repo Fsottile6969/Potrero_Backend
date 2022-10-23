03ebfabaed553cd73eebf6e724f430dca46db6bb
Francisco Sottile <fsottile6969@gmail.com>
2022-10-22 22:11:38 -0300

    correcciones

--- tienda/listar.php
+++ tienda/listar.php
@@ -7,7 +7,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Document</title>
 </head>
-<body class="body overflow-auto">
+<body>
 	<h1 class="titlee text-center">Tienda de ropa</h1>
 	<div class="row">
 		<div class="col d-flex flex-row">
@@ -26,7 +26,7 @@
 			<a role="button" class="btn btn-secondary ms-2" href="index.html">Borrar</a>
 		</div>
 	</div>
-	<div class="row d-flex flex-nowrap justify-content-center">
+	<div class="row d-flex flex-nowrap justify-content-center overflow-hidden">
 		<div class="col d-flex flex-wrap px-1 m-4 justify-content-center" style="max-width: 1184px">
 			<?php
 			//Conexión con la base de datos
