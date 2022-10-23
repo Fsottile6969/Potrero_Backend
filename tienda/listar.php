befd27a4189c2267679ba9d7efde87aeba17615d
Francisco Sottile <fsottile6969@gmail.com>
2022-10-22 22:24:57 -0300

    correcciones

--- tienda/listar.php
+++ tienda/listar.php
@@ -7,9 +7,9 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Document</title>
 </head>
-<body>
+<body style="overflow-x: hidden";>
 	<h1 class="titlee text-center">Tienda de ropa</h1>
-	<div class="row container-fluid">
+	<div class="row">
 		<div class="col d-flex flex-row">
 			<a role="button" class="btn btn-secondary ms-2" href="index.html">Inicio</a>
 			<div class="dropdown ms-2">
